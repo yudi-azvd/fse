61b6521ebc01006cfea06a5d6f9839291a325494
Yudi Yamane <yudiyamane@gmail.com>
2023-05-24 15:44:33 -0300

    feat: escreve inteiro e float

--- src/labs/03-uart/02-modbus/main.cpp
+++ src/labs/03-uart/02-modbus/main.cpp
@@ -6,11 +6,25 @@
 int main() {
     printf("\n");
 
-    i32 i_result = -1;
-    if (modbus_read_int(&i_result) < 0) {
-        perror("Error modbus_read_int");
+    // i32 i_result = -1;
+    // if (modbus_read_int(&i_result) < 0) {
+    //     perror("Error modbus_read_int");
+    // } else {
+    //     printf("read int: %d\n", i_result);
+    // }
+
+    // i32 i_result = 0xa1a1;
+    // if (modbus_write_int(i_result) < 0) {
+    //     perror("Error modbus_write_int");
+    // } else {
+    //     printf("wrote int: %d\n", i_result);
+    // }
+
+    f32 f_result = 3.14159;
+    if (modbus_write_float(f_result) < 0) {
+        perror("Error modbus_write_int");
     } else {
-        printf("read int: %d\n", i_result);
+        printf("wrote float: %.f, %X\n", f_result, f_result);
     }
 
     // f32 f_result = -1;
--- src/labs/03-uart/02-modbus/modbus.h
+++ src/labs/03-uart/02-modbus/modbus.h
@@ -30,27 +30,21 @@
         return fd;                     \
     }
 
-#define USE_COMMON_PACKET_MEMBERS \
-    u8 address;                   \
-    u8 code;                      \
-    u8 subcode;
+#pragma pack(1)
+typedef struct {
+    u8 address;
+    u8 code;
+    u8 data;
+    u16 crc16;
+} BasePacket;
 
 #pragma pack(1)
 typedef struct {
-    USE_COMMON_PACKET_MEMBERS;
+    u8 address;
+    u8 code;
+    u8 data[5];
     u16 crc16;
-} RequestNumberPacket;
-
-#pragma pack(1)
-typedef struct {
-    union data_t {
-        f32 f;
-        i32 i;
-    };
-    USE_COMMON_PACKET_MEMBERS;
-    data_t data;
-    u16 crc16;
-} NumberPacket;
+} WriteNumberPacket;
 
 // typedef struct {
 //     USE_COMMON_PACKET_MEMBERS;
@@ -82,91 +76,113 @@ i32 _modbus_close(int fd) {
 }
 
 i32 modbus_read_int(i32* result) {
-    int return_value = 0;
-    int fd = _modbus_open();
+    i32 return_value = 0;
+    i32 fd = _modbus_open();
     CHECK_FD(fd);
 
-    RequestNumberPacket request_packet;
-    i32 packet_size = sizeof(request_packet);
-    u8* packet_ptr = (u8*)&request_packet;
-    request_packet.address = 0x01;
-    request_packet.code = CODE_READ;
-    request_packet.subcode = SUBCODE_READ_INT;
+    BasePacket packet;
+    i32 packet_size = sizeof(packet);
+    u8* packet_ptr = (u8*)&packet;
+    packet.address = 0x01;
+    packet.code = CODE_READ;
+    packet.data = SUBCODE_READ_INT;
     printf("packet content ");
     print_u8_arr(packet_ptr, packet_size);
 
-    request_packet.crc16 = calculate_crc((u8*)&request_packet, packet_size - sizeof(request_packet.crc16));
-    printf("packet content ");
-    print_u8_arr(packet_ptr, packet_size);
-    printf("         crc16 0x%04X\n", request_packet.crc16);
+    packet.crc16 = calculate_crc((u8*)&packet, packet_size - sizeof(packet.crc16));
+    // printf("packet content ");
+    // print_u8_arr(packet_ptr, packet_size);
+    // printf("         crc16 0x%04X\n", packet.crc16);
 
-    int write_size = write(fd, &request_packet, packet_size);
+    i32 write_size = write(fd, &packet, packet_size);
     if (write_size <= 0) {
         perror("Failed at writing int");
+        return_value = -1;
     }
 
+    // printf("written %d bytes\n", write_size);
     msleep(100);
 
-    NumberPacket np;
-    i32 read_size = read(fd, &np, sizeof(np));
+    WriteNumberPacket p = {0};
+    i32 read_size = read(fd, &p, sizeof(p));
     if (write_size <= 0) {
         perror("Failed at reading int");
+        return_value = -1;
     }
 
-    u16 expected_crc = calculate_crc((u8*)&np, sizeof(np) - sizeof(np.crc16));
-    if (expected_crc != np.crc16) {
+    u16 expected_crc = calculate_crc((u8*)&p, sizeof(p) - sizeof(p.crc16));
+    if (expected_crc != p.crc16) {
+        printf(">> expected crc 0x%04X\n", expected_crc);
+        printf(">>   result crc 0x%04X\n", p.crc16);
         perror("Corrupted number packet");
         return_value = -1;
     }
 
-    *result = np.data.i;
-    printf("result %d\n", *result);
+    printf("writenumber pck\n");
+    printf("packet content ");
+    print_u8_arr(packet_ptr, packet_size);
+
+    // *result = np.data[0];
+    memcpy(result, (&p.data) + 1, 4);
+    printf("(modbus) result %d\n", *result);
 
     _modbus_close(fd);
     return return_value;
 }
 
-i32 modbus_read_float(f32* result) {
-    i32 return_value = 0;
-    i32 fd = _modbus_open();
+i32 modbus_write_int(i32 data) {
+    int return_value = 0;
+    int fd = _modbus_open();
     CHECK_FD(fd);
 
-    RequestNumberPacket p = {0};
+    WriteNumberPacket p = {0};
     i32 packet_size = sizeof(p);
     u8* packet_ptr = (u8*)&p;
-    p.address = 0x01;
-    p.code = CODE_READ;
-    p.subcode = SUBCODE_READ_FLOAT;
-    printf("packet content ");
-    print_u8_arr(packet_ptr, packet_size);
-    p.crc16 = calculate_crc((u8*)&p, packet_size - sizeof(p.crc16));
-    printf("packet content ");
-    print_u8_arr(packet_ptr, packet_size);
-    printf("         crc16 0x%X\n", p.crc16);
 
-    i32 write_size = write(fd, &p, packet_size);
+    p.address = 0x01;
+    p.code = CODE_WRITE;
+    memcpy(&p.data[1], &data, 4);
+    p.data[0] = SUBCODE_WRITE_INT;
+
+    packet_ptr = (u8*)&p.data;
+
+    p.crc16 = calculate_crc((u8*)&p, packet_size - sizeof(p.crc16));
+    packet_ptr = (u8*)&p;
+
+    int write_size = write(fd, &p, packet_size);
     if (write_size <= 0) {
         perror("Failed at writing int");
-    }
-
-    msleep(100);
-
-    NumberPacket np;
-    i32 read_size = read(fd, &np, sizeof(np));
-    if (write_size <= 0) {
-        perror("Failed at reading int");
-    }
-
-    u16 expected_crc = calculate_crc((u8*)&np, sizeof(np) - sizeof(np.crc16));
-    if (expected_crc != np.crc16) {
-        perror("Corrupted number packet");
         return_value = -1;
     }
 
-    *result = np.data.i;
-    printf("result %.2ff\n", *result);
+    return return_value;
+}
 
-    _modbus_close(fd);
+i32 modbus_write_float(f32 data) {
+    int return_value = 0;
+    int fd = _modbus_open();
+    CHECK_FD(fd);
+
+    WriteNumberPacket p = {0};
+    i32 packet_size = sizeof(p);
+    u8* packet_ptr = (u8*)&p;
+
+    p.address = 0x01;
+    p.code = CODE_WRITE;
+    memcpy(&p.data[1], &data, 4);
+    p.data[0] = SUBCODE_WRITE_FLOAT;
+
+    packet_ptr = (u8*)&p.data;
+
+    p.crc16 = calculate_crc((u8*)&p, packet_size - sizeof(p.crc16));
+    packet_ptr = (u8*)&p;
+
+    int write_size = write(fd, &p, packet_size);
+    if (write_size <= 0) {
+        perror("Failed at writing float");
+        return_value = -1;
+    }
+
     return return_value;
 }
 
